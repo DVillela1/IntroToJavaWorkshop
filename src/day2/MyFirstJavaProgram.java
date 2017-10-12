8f76011e493cff1322bca108883a2202d6792906
DVillela1 <league@WTS20.attlocal.net>
2017-10-11 19:46:07 -0700

    DAY 2

--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,17 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+	Robot r2d2 = new Robot();
+	r2d2.penDown();
+	r2d2.setPenColor(Color.PINK);
+	r2d2.setSpeed(10);
+	for (int i = 0; i < 4; i++) {
 		
+	
+		r2d2.move(1);
+		r2d2.turn(360/4);
+		
+	}
 		
 	}
 }
--- src/day2/RobotGraffiti.java
+++ src/day2/RobotGraffiti.java
@@ -1,11 +1,25 @@
 package day2;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class RobotGraffiti {
 	public static void main(String[] args) {
 		// 1.  Create a new Robot
-		
+		Robot r2d2 = new Robot();
 		// 2. Make the Robot draw the first letter of your name
-		
+		r2d2.setSpeed(10);
+		r2d2.penDown();
+		r2d2.setRandomPenColor();
+r2d2.move(99);
+r2d2.turn(180);
+r2d2.move(99*2);
+r2d2.turn(270);
+r2d2.move(90);
+r2d2.turn(299);
+r2d2.move(110);
+r2d2.turn(299*2);
+r2d2.turn(45);
+r2d2.move(200);
 	}
 
 }
